12c1be0d23dd172cf690c39bc30d88e0f3691380
Abbas Hasan <abbashasan@abbass-mbp.lan>
2022-05-21 21:11:18 -0400

    simple hi there

---
+++ Section1/jsx/src/index.js
@@ -0,0 +1,15 @@
+// Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+// Create a react component
+const App = () => {
+  return <div>Hi there!</div>;
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
