231f95ebf6a648c36279db6e56f29b2df7f4db12
Abbas <ahasan62@uwo.ca>
2023-01-14 16:43:31 -0500

    video 46-simple react states

--- section4/src/App.js
+++ section4/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [count,setCount] = useState(0)
+  const handleClick = () => {
+    setCount(count+1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <button onClick={handleClick}>Add animal</button>
+      <p>Animals to show: {count}</p>
     </div>
   );
 }
