aaf0e347886992a6a149dbc2e0d60ce70d5aec7d
Abbas <ahasan62@uwo.ca>
2023-01-16 16:53:58 -0500

    Video 64: get api working

--- section5/src/index.js
+++ section5/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
+import searchImages from './api.js';
 import App from './App.js'
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
-root.render(<App></App>)
+searchImages();
+
+
+
+
+root.render(<App></App>);
