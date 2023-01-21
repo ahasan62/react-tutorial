5c39aa4990826e81094c232a9691338a08c0d932
Abbas <ahasan62@uwo.ca>
2023-01-21 16:43:11 -0500

    added a form, input - onChange, of the text input we will get handleCreate, which will set the title to with the usestate title to the events target value which is the book

--- section6/src/App.js
+++ section6/src/App.js
@@ -1,9 +1,16 @@
 import { useState } from "react";
-
+import BookCreate from "./components /BookCreate"
 function App() {
 
     const [books,setBook] = useState([]);
 
-    return  <div> App </div>
+    const createBook = (title) => {
+        console.log('Need to add book with:',title);
+    };
+
+
+    return  <div> 
+        <BookCreate onCreate={createBook} />
+         </div>
 }
 export default App;
--- section6/src/components /BookCreate.js
+++ section6/src/components /BookCreate.js
@@ -1,4 +1,26 @@
-function BookCreate() {
-    return  <div> BookCreate</div>
+import { useState } from 'react';
+
+
+function BookCreate({onCreate}) {
+    const [title, setTitle] = useState('');
+
+    const handleChange = (event) => { 
+        setTitle(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title);
+
+    }
+    return  <div>
+        <form> 
+            <label>
+                Title
+            </label>
+            <input value={title} onChange={handleChange}/>
+            <button>Create!</button>
+        </form>
+        </div>
 }
 export default BookCreate;
