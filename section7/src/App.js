e827acf807c338fc9a0e7394e30bfdf38f4ff061
Abbas <ahasan62@uwo.ca>
2023-01-23 18:06:23 -0500

    finished

--- section7/src/App.js
+++ section7/src/App.js
@@ -27,7 +27,8 @@ function App() {
         })
         setBook(updatedBooks);
     }
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`)
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
         });
