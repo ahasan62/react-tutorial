7636d3c4eb45a90549d6c08b2d70ea0dc01d648d
Abbas <ahasan62@uwo.ca>
2023-01-23 17:02:55 -0500

    the problem with sec6, is in our page - once u refresh everyhting is gone. Sec7 will teach you how to prevent that

--- section7/src/App.js
+++ section7/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import BookCreate from "./components /BookCreate"
+import BookList from './components /BookList'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+    const [books,setBook] = useState([]);
+
+    const editBookById = (id,newTitle) => {
+        // const editedBooks = book
+        const updatedBooks = books.map((book) => {
+            if (book.id === id){
+                return {...book,title:newTitle};
+            }
+            return book
+        })
+        setBook(updatedBooks);
+    }
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id;
+        });
+        setBook(updatedBooks);
+    }
+    const createBook = (title) => {
+        const updatedBooks = [ 
+            ...books,
+            {id : Math.round(Math.random() * 9999),title: title}
+        ]
+        console.log('Need to add book with:',title);
+        // console.log(books);
+        setBook(updatedBooks);
+    };
+
+
+    return  <div className ="app"> 
+        <BookList onEdit = {editBookById} books={books} onDelete={deleteBookById} />
+        <BookCreate onCreate={createBook} />
+         </div>
+}
 export default App;
